cd303688da103c94816479ebdbb8ecfd866630dd
joymajumder23 <joymajumderjishu@gmail.com>
2024-04-10 01:09:19 +0600

    All categories added and some contents adding to LeftSideNav

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch('./categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, []);
     return (
         <div>
-            <h3 className="text-3xl">Nav</h3>
+            <h3 className="text-xl font-semibold mb-5">All Category{categories.length}</h3>
+            {
+                categories.map(category => <Link
+                    to={`/category/${category.id}`}
+                    className="block text-xl px-12 py-4"
+                    key={category.id}>
+                    {category.name}
+                </Link>)
+            }
         </div>
     );
 };
