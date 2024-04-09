692081755dd6600867d8e5089c98331b78acf875
joymajumder23 <joymajumderjishu@gmail.com>
2024-04-10 00:00:07 +0600

    Components are adding to RightSideNav

--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,7 +1,48 @@
+import { FcGoogle} from "react-icons/fc";
+import { FaGithub, FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 const RightSideNav = () => {
     return (
         <div>
-            <h3 className="text-3xl">Nav</h3>
+            {/* Login With */}
+            <div className="space-y-4 mb-6">
+                <h3 className="text-xl font-semibold">Login With</h3>
+                <div>
+                    <button className="btn btn-block">
+                        <FcGoogle></FcGoogle>
+                        Login with Google
+                    </button>
+                </div>
+                <div>
+                    <button className="btn btn-block">
+                        <FaGithub></FaGithub>
+                        Login with Github
+                    </button>
+                </div>
+            </div>
+
+            {/* Follow Us On */}
+            <div className="mb-5">
+                <h2 className="text-xl font-semibold mb-5">Find Us On</h2>
+                <a className="flex items-center gap-2 p-4 border rounded-t-lg" href="">
+                    <FaFacebook></FaFacebook>
+                    Facebook
+                </a>
+                <a className="flex items-center gap-2 p-4 border-x" href="">
+                    <FaXTwitter></FaXTwitter>
+                    Twitter
+                </a>
+                <a className="flex items-center gap-2 p-4 border rounded-b-lg" href="">
+                    <FaInstagram></FaInstagram>
+                    Facebook
+                </a>
+            </div>
+
+            {/* Q-Zone */}
+            <div>
+                <h2 className="text-xl font-semibold mb-5">Q-Zone</h2>
+            </div>
         </div>
     );
 };
